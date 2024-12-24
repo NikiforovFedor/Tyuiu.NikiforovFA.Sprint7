8467d15262838fe74de2608cb544624b03c7895e
Fedor Nikiforov <Tyuiu.NikiforovFA@outlook.com>
2024-12-24 20:02:18 +0500

    Project.V3

--- Tyuiu.NikiforovFA.Sprint7.Project.V3/FormMain.cs
+++ Tyuiu.NikiforovFA.Sprint7.Project.V3/FormMain.cs
@@ -87,6 +87,7 @@ namespace Tyuiu.NikiforovFA.Sprint7.Project.V3
                 dataGridViewMainGrid_NFA.Columns[4].ValueType = typeof(int);
                 dataGridViewMainGrid_NFA.Columns[5].ValueType = typeof(int);
                 dataGridViewMainGrid_NFA.Columns[6].ValueType = typeof(int);
+
                 dataGridViewMainGrid_NFA.Columns[7].ValueType = typeof(int);
 
                 rows = array.GetLength(0);
